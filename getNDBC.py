5ffe264daeb38759705dd91823a7f5ddbf229b96
Rana <rana@caltech.edu>
2023-12-26 02:59:16 -0800

    got overlay woerkin

--- getNDBC.py
+++ getNDBC.py
@@ -1,87 +1,119 @@
 #!/usr/bin/env python3
 
-"""
-Script Name: getNDBC.py
-Description: This script downloads and processes historical oceanographic data 
-             from the National Data Buoy Center (NDBC). It focuses on extracting 
-             specified data columns for specified buoys and time periods, as 
-             defined in a YAML configuration file.
-
-Author: [Your Name]
-Created on: [Date Created]
-Last Modified: [Last Modification Date]
-
-Usage:
-    Run the script with a YAML configuration file:
-    python3 getNDBC.py --config_file config.yaml --year 2022
-    Or run without specifying a config file to use the default 'config.yaml':
-    python3 getNDBC.py --year 2022
-
-Dependencies:
-    Requires Python 3 and the following libraries: 
-    - requests
-    - pandas
-    - argparse
-    - pyyaml
-"""
-
-import requests
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
 import pandas as pd
-import io
+import numpy as np
 import os
+import glob
 import argparse
 import yaml
 
+# Default data directory
 data_dir = 'Data/'
-
-def download_ndbc_historical_data(buoy_id, year):
-    url = f"https://www.ndbc.noaa.gov/view_text_file.php?filename={buoy_id}h{year}.txt.gz&dir=data/historical/stdmet/"
-    response = requests.get(url)
-    if response.status_code != 200:
-        print(f"Failed to retrieve data for buoy {buoy_id} for {year}")
-        return None
-    return response.content.decode('utf-8')
-
-def parse_data_to_dict(data, columns, months=list(range(1, 12 + 1))):
-    df = pd.read_fwf(io.StringIO(data), skiprows=[1], header=0)
-    df_filtered = df[df['MM'].isin(months)].copy()
-
-    required_columns = ['#YY', 'MM', 'DD', 'hh', 'mm'] + columns
-    for column in required_columns:
-        if column in df.columns:
-            df_filtered.loc[:, column] = df_filtered[column].astype(str).str.zfill(2)
-
-    df_filtered['timestamp'] = pd.to_datetime(df_filtered[['#YY', 'MM', 'DD', 'hh', 'mm']].agg(''.join, axis=1), format='%Y%m%d%H%M')
-    return df_filtered[['timestamp'] + columns]
-
-def save_data(df, buoy_id, year):
-    if not os.path.exists(data_dir):
-        os.makedirs(data_dir)
-    filename_pickle = os.path.join(data_dir, f'BuoyData_{buoy_id}_{year}.pkl')
-    df.to_pickle(filename_pickle, compression='gzip')
 
 def load_config(config_file):
     with open(config_file, 'r') as file:
-        config = yaml.safe_load(file)
-    return config
+        return yaml.safe_load(file)
 
-def main(config_file, year):
+def find_pickle_files(directory):
+    return glob.glob(os.path.join(directory, '*.pkl'))
+
+def load_pickle_data(filename):
+    df = pd.read_pickle(filename, compression='gzip')
+    for col in df.columns:
+        if col != 'timestamp':
+            df[col] = pd.to_numeric(df[col], errors='coerce')
+    return df
+
+def filter_data(df, column):
+    return df[df[column] <= 90]
+
+def determine_y_scales(pickle_files, units_map):
+    global_max = {}
+    global_min = {}
+    for pickle_file in pickle_files:
+        df = load_pickle_data(pickle_file)
+        for column in units_map.keys():
+            if column in df.columns:
+                filtered_df = filter_data(df, column)
+                max_val = filtered_df[column].max()
+                min_val = filtered_df[column].min()
+                global_max[column] = max(global_max.get(column, -np.inf), max_val)
+                global_min[column] = min(global_min.get(column, np.inf), min_val)
+    return global_max, global_min
+
+def plot_individual_data(pickle_files, pdf, y_scales, units_map):
+    global_max, global_min = y_scales
+    for pickle_file in pickle_files:
+        df = load_pickle_data(pickle_file)
+        buoy_id = os.path.basename(pickle_file).split('_')[1]
+        plt.style.use('seaborn')
+        columns_to_plot = [col for col in df.columns if col != 'timestamp']
+        num_plots = len(columns_to_plot)
+
+        fig, axes = plt.subplots(num_plots, 1, figsize=(10, 6*num_plots), sharex=True)
+
+        for i, column in enumerate(columns_to_plot):
+            ax = axes[i] if num_plots > 1 else axes
+            filtered_data = filter_data(df, column)
+            unit = units_map.get(column, "Unknown Unit")
+            ax.plot(filtered_data['timestamp'], filtered_data[column], label=f"{column} over Time")
+            ax.set_ylabel(f"{column} ({unit})")
+            ax.legend()
+
+            if column in global_max and column in global_min:
+                ax.set_ylim(global_min[column], global_max[column])
+
+        plt.xlabel('Time')
+        plt.suptitle(f'Meteorological Data Analysis for Buoy {buoy_id}')
+        pdf.savefig(fig, bbox_inches='tight')
+        plt.close(fig)
+
+def plot_overlayed_data(pickle_files, pdf, units_map):
+    all_data = {}
+    for pickle_file in pickle_files:
+        buoy_id = os.path.basename(pickle_file).split('_')[1]
+        df = load_pickle_data(pickle_file)
+        for column in df.columns:
+            if column != 'timestamp':
+                filtered_data = filter_data(df, column)
+                if column not in all_data:
+                    all_data[column] = {}
+                all_data[column][buoy_id] = filtered_data[['timestamp', column]]
+
+    for column, data in all_data.items():
+        fig, ax = plt.subplots(figsize=(10, 6))
+        for buoy_id, df in data.items():
+            ax.plot(df['timestamp'], df[column], label=f"Buoy {buoy_id}")
+        ax.set_ylabel(f"{column} ({units_map.get(column, 'Unknown Unit')})")
+        ax.legend()
+        plt.xlabel('Time')
+        plt.title(f'{column} Data from Multiple Buoys')
+        pdf.savefig(fig, bbox_inches='tight')
+        plt.close(fig)
+
+def main(directory, overlay, config_file):
     config = load_config(config_file)
-    buoy_ids = config['buoys']
-    columns = config['columns']
+    units_map = config['units_map']
 
-    for buoy_id in buoy_ids:
-        data = download_ndbc_historical_data(buoy_id, year)
-        if data:
-            df = parse_data_to_dict(data, columns)
-            save_data(df, buoy_id, year)
-            print(f"Data processed and saved for buoy {buoy_id} for {year}")
+    figures_directory = 'Figures'
+    os.makedirs(figures_directory, exist_ok=True)
+    pdf_path = os.path.join(figures_directory, 'buoy_data_plots.pdf')
+    
+    pickle_files = find_pickle_files(directory)
+    with PdfPages(pdf_path) as pdf:
+        if overlay:
+            plot_overlayed_data(pickle_files, pdf, units_map)
+        else:
+            y_scales = determine_y_scales(pickle_files, units_map)
+            plot_individual_data(pickle_files, pdf, y_scales, units_map)
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Download and process NDBC buoy data.")
-    parser.add_argument("--config_file", type=str, default='config.yaml',
-                         help="YAML configuration file. Default is 'config.yaml'.")
-    parser.add_argument("--year", type=int, default=2022, 
-                        help="The year for which to download data.")
+    parser = argparse.ArgumentParser(description="Plot NDBC buoy data.")
+    parser.add_argument("directory", type=str, nargs='?', default=data_dir, help="Directory containing Pickle files. Default is 'Data/'.")
+    parser.add_argument("--overlay", action='store_true', help="Overlay data from all buoys on single plots.")
+    parser.add_argument("--config", type=str, default="config.yaml", help="Path to the configuration YAML file. Default is 'config.yaml'.")
     args = parser.parse_args()
-    main(args.config_file, args.year)
+
+    main(args.directory, args.overlay, args.config)
--- plotBuoys.py
+++ plotBuoys.py
@@ -1,33 +1,25 @@
+#!/usr/bin/env python3
 
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
 import pandas as pd
+import seaborn as sns
 import numpy as np
 import os
 import glob
 import argparse
 
-# Default data directory
-data_dir = 'Data/'
+# Enhanced plot style settings
+plt.style.use('seaborn-v0_8-darkgrid')
+sns.set_context('talk')  # Larger plot elements suitable for presentations
 
 # Units mapping based on standard NDBC measurements
 units_map = {
-    'WDIR': 'degrees (True)',
-    'WSPD': 'm/s',
-    'GST': 'm/s',
-    'WVHT': 'm',
-    'DPD': 's',
-    'APD': 's',
-    'MWD': 'degrees (True)',
-    'PRES': 'hPa',
-    'ATMP': '°C',
-    'WTMP': '°C',
-    'DEWP': '°C',
-    'VIS': 'nmi',
-    'PTDY': 'hPa',
-    'TIDE': 'ft',
-    # Additional mappings can be added here as needed
+    # ... (your units mapping here)
 }
+
+# Default data directory
+data_dir = 'Data/'
 
 def find_pickle_files(directory):
     return glob.glob(os.path.join(directory, '*.pkl'))
@@ -39,6 +31,9 @@ def load_pickle_data(filename):
             df[col] = pd.to_numeric(df[col], errors='coerce')
     return df
 
+def filter_data(df, column):
+    return df[df[column] <= 90]
+
 def determine_y_scales(pickle_files):
     global_max = {}
     global_min = {}
@@ -46,28 +41,30 @@ def determine_y_scales(pickle_files):
         df = load_pickle_data(pickle_file)
         for column in df.columns:
             if column != 'timestamp':
-                max_val = df[df[column] <= 90][column].max()
-                min_val = df[df[column] <= 90][column].min()
+                filtered_df = filter_data(df, column)
+                max_val = filtered_df[column].max()
+                min_val = filtered_df[column].min()
                 global_max[column] = max(global_max.get(column, -np.inf), max_val)
                 global_min[column] = min(global_min.get(column, np.inf), min_val)
     return global_max, global_min
 
 def plot_individual_data(pickle_files, pdf, y_scales):
     global_max, global_min = y_scales
-    for pickle_file in pickle_files:
+    color_palette = sns.color_palette('husl', len(pickle_files))  # Use a pleasing color palette
+
+    for i, pickle_file in enumerate(pickle_files):
         df = load_pickle_data(pickle_file)
         buoy_id = os.path.basename(pickle_file).split('_')[1]
-        plt.style.use('bmh')
         columns_to_plot = [col for col in df.columns if col != 'timestamp']
         num_plots = len(columns_to_plot)
 
         fig, axes = plt.subplots(num_plots, 1, figsize=(10, 6*num_plots), sharex=True)
 
-        for i, column in enumerate(columns_to_plot):
-            ax = axes[i] if num_plots > 1 else axes
-            filtered_data = df[df[column] <= 90]
+        for j, column in enumerate(columns_to_plot):
+            ax = axes[j] if num_plots > 1 else axes
+            filtered_data = filter_data(df, column)
             unit = units_map.get(column, "Unknown Unit")
-            ax.plot(filtered_data['timestamp'], filtered_data[column], label=f"{column} over Time")
+            ax.plot(filtered_data['timestamp'], filtered_data[column], label=f"{column} over Time", color=color_palette[i])
             ax.set_ylabel(f"{column} ({unit})")
             ax.legend()
 
@@ -81,19 +78,22 @@ def plot_individual_data(pickle_files, pdf, y_scales):
 
 def plot_overlayed_data(pickle_files, pdf, units_map):
     all_data = {}
+    color_palette = sns.color_palette('husl', len(pickle_files))  # Use a pleasing color palette
+
     for pickle_file in pickle_files:
         buoy_id = os.path.basename(pickle_file).split('_')[1]
         df = load_pickle_data(pickle_file)
         for column in df.columns:
             if column != 'timestamp':
+                filtered_data = filter_data(df, column)
                 if column not in all_data:
                     all_data[column] = {}
-                all_data[column][buoy_id] = df[['timestamp', column]]
+                all_data[column][buoy_id] = filtered_data[['timestamp', column]]
 
     for column, data in all_data.items():
         fig, ax = plt.subplots(figsize=(10, 6))
-        for buoy_id, df in data.items():
-            ax.plot(df['timestamp'], df[column], label=f"Buoy {buoy_id}")
+        for i, (buoy_id, df) in enumerate(data.items()):
+            ax.plot(df['timestamp'], df[column], label=f"Buoy {buoy_id}", color=color_palette[i])
         ax.set_ylabel(f"{column} ({units_map.get(column, 'Unknown Unit')})")
         ax.legend()
         plt.xlabel('Time')
@@ -109,19 +109,15 @@ def main(directory, overlay):
     pickle_files = find_pickle_files(directory)
     with PdfPages(pdf_path) as pdf:
         if overlay:
-            print("Plot Overlayed Data...")
             plot_overlayed_data(pickle_files, pdf, units_map)
         else:
-            print("1 page per Buoy...")
             y_scales = determine_y_scales(pickle_files)
             plot_individual_data(pickle_files, pdf, y_scales)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Plot NDBC buoy data.")
-    parser.add_argument("directory", type=str, nargs='?', default=data_dir, 
-                        help="Directory containing Pickle files. Default is 'Data/'.")
-    parser.add_argument("--overlay", action='store_true', 
-                        help="Overlay data from all buoys on single plots.")
+    parser.add_argument("directory", type=str, nargs='?', default=data_dir, help="Directory containing Pickle files. Default is 'Data/'.")
+    parser.add_argument("--overlay", action='store_true', help="Overlay data from all buoys on single plots.")
     args = parser.parse_args()
 
     main(args.directory, args.overlay)
